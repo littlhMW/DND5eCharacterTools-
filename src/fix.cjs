b4176a6350f12e9a465d0e8da712e04a8de65619
littlh <117704805+littlhMW@users.noreply.github.com>
2026-05-21 02:28:22 +0800

    Delete src directory

--- src/fix.cjs
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let c = fs.readFileSync('src/data/classes.ts', 'utf8');
-const regex = /战斗大师范型包含了一些在战斗以外的专业，像是武器锻造和书法等等。',[\s\S]*?traits:/;
-c = c.replace(regex, "战斗大师范型包含了一些在战斗以外的专业，像是武器锻造和书法等等。',\n        traits:");
-fs.writeFileSync('src/data/classes.ts', c);
--- src/src/data/fillData.ts
+++
@@ -1,1 +0,0 @@
-export const buildDataFiles = `Run this if needed`;
